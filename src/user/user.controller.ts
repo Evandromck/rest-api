289fa297058f9cd06fc017090be916ea8378ddd2
Evandromck <evandroaraujomck@gmail.com>
2023-02-23 13:23:57 -0300

    Deploy rotas controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Patch, Head, Options } from '@nestjs/common';
+import { User as UserModel } from '@prisma/client'
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    constructor (private readonly userService: UserService){}
+
+    //: Promise<UserModel[]>
+
+    @Get()
+    async getUsers(){
+        return "Executando via get, recuperando todos os usuários";
+    }
+
+    @Post()
+     async createUser(){
+        return 'Executando via Post,';
+
+     }
+
+     @Put()
+     async updateUser(){
+        return 'executando operação via put';
+     }
+
+     @Delete()
+     async DeleteUser(){
+        return'Deletando informação';
+     }
+
+     @Delete(":id")
+     async getUser(){
+        return 'executando via get id';
+     }
+
+     @Patch()
+     async patUser(){
+        return "executando via PATCH";
+     }
+
+     @Head()
+      async headUser(){
+        return "executando via head";
+      }
+
+      @Options()
+      async optUser(){
+        return "executando via options"
+      }
+
+
+}
